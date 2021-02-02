ee24d6f496250a88735bb43383837e8aece023cb
Kevin Tran <kevin115533@yahoo.com>
2021-02-01 19:50:33 -0600

    ITD3333
    Lab work - error handling

--- ITD3333Week4/Default.aspx
+++ ITD3333Week4/Default.aspx
@@ -9,6 +9,14 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            A:
+            <asp:TextBox ID="txtBoxA" runat="server"></asp:TextBox><br />
+            B:
+            <asp:TextBox ID="txtBoxB" runat="server"></asp:TextBox><br />
+            <br />
+            <asp:Button ID="btnDivide" runat="server" OnClick="cmdCompute_Click" Text="Divide A / B" />
+            <br />
+            <asp:Label ID="lblResult" runat="server" Text=""></asp:Label>
         </div>
     </form>
 </body>
--- ITD3333Week4/Default.aspx.cs
+++ ITD3333Week4/Default.aspx.cs
@@ -11,4 +11,25 @@ public partial class _Default : System.Web.UI.Page
     {
 
     }
+    protected void cmdCompute_Click(Object sender, EventArgs e)
+    {
+        try {
+            decimal a, b, result;
+            a = decimal.Parse(txtBoxA.Text);
+            b = decimal.Parse(txtBoxB.Text);
+            result = a / b;
+            lblResult.Text = result.ToString();
+            lblResult.ForeColor = System.Drawing.Color.Black; 
+        }  
+        catch(Exception err)
+        {
+            lblResult.Text = "<b>Message:</b> " + err.Message;
+            lblResult.Text += "<br></br> ";
+            lblResult.Text += "<b>Source:</b> " + err.Source;
+            lblResult.Text += "<br></br> ";
+            lblResult.Text += "<b>Stack Trace:</b> " + err.StackTrace;
+            lblResult.ForeColor = System.Drawing.Color.Red;
+        }
+    }
+    
 }
