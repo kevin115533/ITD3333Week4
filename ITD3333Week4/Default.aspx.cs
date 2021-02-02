e4d22bfd592012188346be5d9d74730b3e3e20ba
Kevin Tran <kevin115533@yahoo.com>
2021-02-01 22:03:43 -0600

    ITD 3333 Lab
    error handling

--- ITD3333Week4/Default.aspx.cs
+++ ITD3333Week4/Default.aspx.cs
@@ -7,29 +7,34 @@ using System.Web.UI.WebControls;
 
 public partial class _Default : System.Web.UI.Page
 {
+   
+
     protected void Page_Load(object sender, EventArgs e)
     {
-
+     
     }
-    protected void cmdCompute_Click(Object sender, EventArgs e)
+    private void DivideNumbers()
     {
-        try {
-            decimal a, b, result;
-            a = decimal.Parse(txtBoxA.Text);
-            b = decimal.Parse(txtBoxB.Text);
-            result = a / b;
-            lblResult.Text = result.ToString();
-            lblResult.ForeColor = System.Drawing.Color.Black; 
-        }  
-        catch(Exception err)
+        decimal numerator = decimal.Parse(txtBoxA.Text);
+        decimal divisor = decimal.Parse(txtBoxB.Text);
+        decimal result;
+        try
         {
-            lblResult.Text = "<b>Message:</b> " + err.Message;
-            lblResult.Text += "<br></br> ";
-            lblResult.Text += "<b>Source:</b> " + err.Source;
-            lblResult.Text += "<br></br> ";
-            lblResult.Text += "<b>Stack Trace:</b> " + err.StackTrace;
+            result = numerator / divisor;
+            lblResult.Text = result.ToString();
+            lblResult.ForeColor = System.Drawing.Color.Black;
+        }
+        catch (DivideByZeroException)
+        {
+            DivideByZeroException KevinTranException = new DivideByZeroException("ITD1333-1940: Divide by Zero");
+            lblResult.Text = "Student Name: Kevin Tran<br></br>";
+            lblResult.Text += KevinTranException.Message;
             lblResult.ForeColor = System.Drawing.Color.Red;
         }
     }
-    
+    protected void cmdCompute_Click(Object sender, EventArgs e)
+    {
+        DivideNumbers();
+    }
+
 }
